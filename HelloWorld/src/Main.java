2aff33ecf5e0056f17f071d6888a3023383effa8
Vo Tuan Dat <dattuanvo1402@gmail.com>
2026-02-02 12:12:30 +0700

    debug

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Xin chào, Tôi là Sinh viên Trường đại học Nha Trang");
     }
